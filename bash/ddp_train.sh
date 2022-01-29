fe21b1e641d0a2e28164a54f1a1f6bd650016ac2
Dong-Hee Kim <donghee.kim@vuno.co>
2022-01-29 22:08:50 +0900

    modify train example shell

--- bash/ddp_train.sh
+++ bash/ddp_train.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 cd ../
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python run.py trainer.gpus=7 +trainer.num_nodes=1 +trainer.strategy=ddp +trainer.precision=16 \
-                                                    datamodule.data_dir=/nfs2/personal/cmpark/dacon/dataset/train \
-                                                    datamodule.crop=False \
-                                                    datamodule.batch_size=30 \
-                                                    datamodule.label_type=positive \
-                                                    model.model.num_classes=19 \
-                                                    model.model.name=swin_large_patch4_window7_224_in22k
-                                                    
+# CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7 python run.py trainer.gpus=7 +trainer.num_nodes=1 +trainer.strategy=ddp +trainer.precision=16 \
+#                                                     datamodule.data_dir=/nfs2/personal/cmpark/dacon/dataset/train \
+#                                                     datamodule.crop=False \
+#                                                     datamodule.batch_size=30 \
+#                                                     datamodule.label_type=positive \
+#                                                     model.model.num_classes=19 \
+#                                                     model.model.name=swin_large_patch4_window7_224_in22k
+
+for fold in 0 1 2 3 4
+do
+    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python run.py datamodule.batch_size=8 trainer.gpus=8 +trainer.num_nodes=1 +trainer.strategy=ddp +trainer.precision=32 \
+                                                        datamodule.data_dir=/pathos2/nfs2/personal/cmpark/dacon/dataset/train \
+                                                        datamodule.label_type=total datamodule.fold=$fold \
+                                                        model.model.num_classes=25 model.lr=1e-2 trainer.max_epochs=100 model.SAM=True model.label_smoothing=0.05
+                                                        # datamodule.crop=True \
+                                                        # /pathos2/data2/dongheekim/Plant/dacon-template/data/train
+done
                                                     
--- bash/train.sh
+++ bash/train.sh
@@ -1,5 +1,7 @@
 #!bin/bash
 cd ../
 CUDA_VISIBLE_DEVICES=4 python run.py trainer.gpus=1 datamodule.batch_size=1 +trainer.precision=16 logger=wandb \
-                                                    datamodule.data_dir=/pathos2/data2/dongheekim/Plant/dacon-template/data/train \
-                                                    datamodule.crop=True
+                                                    datamodule.data_dir=/pathos2/nfs2/personal/cmpark/dacon/dataset/train \
+                                                    datamodule.label_type=total model.lr=1e-4 \
+                                                    model.model.num_classes=25
+                                                    # datamodule.crop=True
